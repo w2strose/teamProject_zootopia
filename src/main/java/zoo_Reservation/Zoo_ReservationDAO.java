92420654cb0e006210e685b0caa217e9afed7473
USER <USER@DESKTOP-DFNHV3R>
2021-12-30 18:18:24 +0900

    zoo_reser 생성

---
+++ src/main/java/zoo_Reservation/Zoo_ReservationDAO.java
@@ -0,0 +1,58 @@
+package zoo_Reservation;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+public class Zoo_ReservationDAO {
+
+	private Connection getConnection() {// DBCP 
+		
+		Connection con = null;
+		
+		try {
+			InitialContext ctx = new InitialContext();	
+			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/mydb");
+			con = ds.getConnection();
+		}catch(Exception e) {
+			System.out.println("Connection 실패");
+		}
+		return con;
+
+	}
+	
+	
+	public void reservationSearch(int year, int month, int day) {
+		
+		Connection con = null;
+		PreparedStatement pstmt = null; // 바인딩변수
+		ResultSet rs = null;
+		
+		try {
+			con = getConnection();
+			
+			String strQuery = "select * from zoomember where id=?";
+			pstmt = con.prepareStatement(strQuery);
+			//pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+	
+			if(rs.next()) {// 아이디에 해당하는 회원이 존재한다면 
+				/*vo = new zoomemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setPass(rs.getString("pass"));
+				vo.setName(rs.getString("name"));
+				vo.setPhone1(rs.getString("phone1"));
+				vo.setPhone2(rs.getString("phone2"));
+				vo.setPhone3(rs.getString("phone3"));
+				vo.setEmail(rs.getString("email"));
+				vo.setBirth(rs.getString("birth"));*/
+			}// end of if
+			
+		}catch(Exception e) {	}
+		
+	}
+}
--- src/main/webapp/zootopia/reservation.jsp
+++ src/main/webapp/zootopia/reservation.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.sql.Connection"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page import="java.util.Calendar" %>
@@ -28,6 +29,7 @@
 	int endDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 	int week = cal.get(Calendar.DAY_OF_WEEK);
 	
+		
 %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
